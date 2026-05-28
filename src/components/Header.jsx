bdc691f7253d501be6a1cb8396ceb0ed94aaa01a
Gourab Neogi <gourabneogi7775@gmail.com>
2026-05-29 00:59:17 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import { useParams } from "wouter";
+import { useParams, useLocation } from "wouter";
 import { getStoredSlug } from "../utils/constants";
 import { HamburgerMenu } from "./HamburgerMenu";
-import { useGoBack } from "../context/NavigationContext";
 
 export function Header() {
   const { slug: urlSlug } = useParams();
   const slug = urlSlug || getStoredSlug();
-  const goBack = useGoBack(`/${slug}`);
+  const [, navigate] = useLocation();
+
+  const goToLanding = () => {
+    const qs = window.location.search;
+    navigate(`/${slug}${qs}`);
+  };
 
   return (
     <header className="header">
-      <button className="iconBtn pressable" onClick={goBack} aria-label="Back">
+      <button className="iconBtn pressable" onClick={goToLanding} aria-label="Back">
         ←
       </button>
       <HamburgerMenu slug={slug} />
